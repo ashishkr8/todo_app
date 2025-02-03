3de51c5643b2b1386442e53299dc852b8b0d8bb5
ashishkr8 <ashish_kumar8@epam.com>
2025-02-04 00:05:56 +0530

    redux added

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment, incrementByAmount } from './features/counter/counterSlice'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)?
+  const count = useSelector(state=>state.counter.value);
+  const dispatch = useDispatch();
 
   return (
     <>
@@ -17,9 +21,16 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <p>count is {count}</p>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => dispatch(increment())}>
+          increment
+        </button>
+        <button onClick={() => dispatch(decrement())}>
+          decrement
+        </button>
+        <button onClick={() => dispatch(incrementByAmount(5))}>
+          increment by 5
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
