de7ba5c42322e7d345cfedff5ce2a88212d269fb
ashishkr8 <ashish_kumar8@epam.com>
2025-02-04 01:58:45 +0530

    counter-added

--- src/components/Todoinput.jsx
+++ src/components/Todoinput.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 import { IoMdAdd } from "react-icons/io";
 import { addItem } from '../features/todo/todolistSlice';
+import { increment } from '../features/counter/counterSlice';
 import { useDispatch } from 'react-redux';
 
 const Todoinput = () => {
@@ -25,14 +26,15 @@ const Todoinput = () => {
         id:id,
         value:input,
       }
-      dispatch(addItem(todoObj))
+      dispatch(addItem(todoObj));
+      dispatch(increment());
       setInput(prev=>"");
     }
   return (
-    <div className='flex flex-row items-center justify-center'>
-        <input type="text" onChange={handleTodoInput} value={input} className='bg-gray-200 p-1'/>
+    <div className='flex flex-row items-center justify-center bg-gray-200 p-1'>
+        <input type="text" onChange={handleTodoInput} value={input} className=''/>
      
-        <IoMdAdd onClick={handleAddTodo} className='text-green-500'/>
+        <IoMdAdd onClick={handleAddTodo} className='text-green-500 hover:cursor-pointer'/>
       
     </div>
   )
--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -1,23 +1,31 @@
 import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { deleteItem } from '../features/todo/todolistSlice'
+import { decrement } from '../features/counter/counterSlice';
 import { MdOutlineDeleteForever } from "react-icons/md";
+import { GoChecklist } from "react-icons/go";
 
 const Todolist = () => {
     const todolist = useSelector((state)=>(state.todolist.todolist));
+    const count = useSelector(state => state.counter.value);
     const dispatch = useDispatch();
 
     const handleDelete = (id) => {
         dispatch(deleteItem(id));
+        dispatch(decrement());
        
     }
   return (
-    <div className='mt-4 bg-gray-700 text-white'>
+    <div className=''>
+        <p className='flex flex-row items-center justify-center m-2'>
+        <GoChecklist />
+        {count}
+        </p>
         {todolist.map(item=>
-            <div key={item.id} className='flex flex-row items-center justify-between gap-2 p-2'>
+            <div key={item.id} className='flex flex-row items-center justify-between gap-2 p-2 mt-4 bg-gray-700 text-white'>
                 <p>{item.value}</p>
                 
-                <MdOutlineDeleteForever onClick={()=>handleDelete(item.id)} className='text-red-500'/>
+                <MdOutlineDeleteForever onClick={()=>handleDelete(item.id)} className='text-red-500 hover:cursor-pointer'/>
             </div>
         )}
     </div>
